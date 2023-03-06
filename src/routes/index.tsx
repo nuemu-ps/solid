ec68591c9f2918688f7eaae83a68f7bbd2ae5bd6
nuemu-ps <n.uemura.ps@gmail.com>
2023-03-06 21:25:34 +0900

    add: serverData

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,34 @@
 import { createSignal, For, Show } from "solid-js";
-import { Title } from "solid-start";
+import { Title, useRouteData } from "solid-start";
+import { createServerData$, createServerAction$ } from "solid-start/server";
 import Block from "~/components/Block";
 import DropDownMenu from "~/components/DropDownMenu";
 
-export default function Home() {
-  const [array, setArray] = createSignal(
+export const routeData = () => {
+  return createServerData$(() =>
     Array(1000)
       .fill({ type: "Text", data: "TextBlock" })
       .map((block, index) => {
         return { type: `${block.type}`, data: `${block.data}-${index}` };
       })
   );
+};
+
+export default function Home() {
+  const data = useRouteData<typeof routeData>();
+  const [array, setArray] = createSignal(data());
+
+  const [, postBlocks] = createServerAction$(async (new_blocks: any) => {
+    console.log(new_blocks);
+  });
+
   const [focusing, setFocusing] = createSignal(-1);
 
   const add = (index: number) => {
     const newArray = JSON.parse(JSON.stringify(array()));
     newArray.splice(index + 1, 0, { type: "Text", data: "TextBlock" });
     setArray(newArray);
+    postBlocks(newArray);
   };
 
   const destroy = (index: number) => {
